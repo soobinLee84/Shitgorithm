ac3b9ffdcf413b991469c2738a2165704dad154e
Jungea <jungea2241@gmail.com>
2024-01-05 16:35:48 +0900

    💩 [BOJ] 1655.가운데를 말해요 / 시간초과2

--- src/BOJ/BOJ1655.java
+++ src/BOJ/BOJ1655.java
@@ -3,7 +3,6 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,16 +12,33 @@ public class BOJ1655 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+
         List<Integer> list = new LinkedList<>();
+        list.add(Integer.parseInt(br.readLine()));
+        System.out.println(getCenter(list));
 
         int input;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 1; i++) {
+            System.out.println(list);
             input = Integer.parseInt(br.readLine());
-            list.add(input);
-            Collections.sort(list);
+            list.add(addIndex(list, input), input);
             System.out.println(getCenter(list));
-
         }
+
+    }
+
+    private static int addIndex(List<Integer> list, int n) {
+
+        int index = 0;
+
+        while (list.size() > index) {
+            if (list.get(index) > n) {
+                break;
+            }
+            index++;
+        }
+
+        return index;
     }
 
     public static int getCenter(List<Integer> list) {
