be849dce5c15f4b09a97f8a22c57bf414ab3968e
Jungea <jungea2241@gmail.com>
2024-01-10 13:38:35 +0900

    ✅ [PGS] 76501.음양 더하기

---
+++ src/PGS/PGS76501.java
@@ -0,0 +1,20 @@
+package PGS;
+
+public class PGS76501 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{4, 7, 12}, new boolean[]{true, false, true}));  //9
+        System.out.println(solution(new int[]{1, 2, 3}, new boolean[]{false, false, true}));  //0
+    }
+
+    public static int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for (int i = 0; i < absolutes.length; i++) {
+            answer += absolutes[i] * (signs[i] ? 1 : -1);
+        }
+
+        return answer;
+    }
+
+}
