d3f37dae5169384bbe9fc6b4dfb377fbac197f87
Jungea <jungea2241@gmail.com>
2024-01-13 23:46:05 +0900

    ✅ [PGS] 12969.직사각형 별찍기

---
+++ src/PGS/PGS12969.java
@@ -0,0 +1,22 @@
+package PGS;
+
+import java.util.Scanner;
+
+public class PGS12969 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
