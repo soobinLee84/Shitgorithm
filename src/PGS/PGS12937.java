0f794a381b2941b40db2a76e2883defab03ac875
Jungea <jungea2241@gmail.com>
2024-01-15 10:02:14 +0900

    ✅ [PGS] 12937.짝수와 홀수

---
+++ src/PGS/PGS12937.java
@@ -0,0 +1,14 @@
+package PGS;
+
+public class PGS12937 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(3));
+        System.out.println(solution(4));
+    }
+
+    public static String solution(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+
+}
