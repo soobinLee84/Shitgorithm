7eda3402f5ba861f8d9f4c0f7e03266e776eeb9f
Jungea <jungea2241@gmail.com>
2024-01-15 17:51:31 +0900

    ✅ [PGS] 12931.자릿수 더하기

---
+++ src/PGS/PGS12931.java
@@ -0,0 +1,19 @@
+package PGS;
+
+public class PGS12931 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(123));
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+
+        while (n > 0) {
+            answer += (n % 10);
+            n /= 10;
+        }
+
+        return answer;
+    }
+}
