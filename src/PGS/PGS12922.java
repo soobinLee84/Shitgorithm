088c051e11314dc7958f6b5cf08f823c74716c03
Jungea <jungea2241@gmail.com>
2024-01-16 15:58:34 +0900

    ✅ [PGS] 12922.수박수박수박수박수박수?

---
+++ src/PGS/PGS12922.java
@@ -0,0 +1,25 @@
+package PGS;
+
+public class PGS12922 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(3));
+        System.out.println(solution(4));
+    }
+
+    public static String solution(int n) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n / 2; i++) {
+            sb.append("수박");
+        }
+
+        if (n % 2 == 1) {
+            sb.append("수");
+        }
+
+        return sb.toString();
+    }
+
+}
