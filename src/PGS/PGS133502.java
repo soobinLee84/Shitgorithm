89a7fe8e14333239ba519f5993d9185370317ea2
Jungea <jungea2241@gmail.com>
2024-02-18 13:13:25 +0900

    ✅ [PGS] 133502.햄버거 만들기

--- src/PGS/PGS133502.java
+++ src/PGS/PGS133502.java
@@ -1,31 +1,39 @@
 package PGS;
 
-import java.util.Arrays;
+import java.util.Stack;
 
 public class PGS133502 {
     public static void main(String[] args) {
         System.out.println(solution(new int[]{2, 1, 1, 2, 3, 1, 2, 3, 1}));
+//        System.out.println(solution(new int[]{1, 3, 2, 1, 2, 1, 3, 1, 2}));
     }
 
     public static int solution(int[] ingredient) {
         int answer = 0;
 
-        StringBuilder sb = new StringBuilder(Arrays.toString(ingredient));
-        sb.delete(0, 1);
-        sb.delete(sb.length() - 1, sb.length());
-        sb.append(", ");
+        Stack<Integer> stack = new Stack<>();
 
-        int index = -1;
-        while (true) {
-            index = sb.indexOf("1, 2, 3, 1, ");
+        int[] a = new int[4];
+        for (int i : ingredient) {
+            stack.push(i);
 
-            if (index == -1) {
-                break;
+            if (stack.size() > 3) {
+                a[3] = stack.pop();
+                a[2] = stack.pop();
+                a[1] = stack.pop();
+                a[0] = stack.pop();
+
+                if (a[0] == 1 && a[1] == 2 && a[2] == 3 && a[3] == 1) {
+                    answer++;
+                } else {
+                    stack.push(a[0]);
+                    stack.push(a[1]);
+                    stack.push(a[2]);
+                    stack.push(a[3]);
+                }
             }
-
-            sb.delete(index, index + 12);
-            answer++;
         }
+
         return answer;
     }
 }
